b62673a429da6f33479f95f7ef763b307a269ecb
Gagan021-5 <gagansingh010221@gmail.com>
2025-12-03 22:54:29 +0530

    revisited longpressed questions

---
+++ longpressedname.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class longpressedname {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isLongPressedName(String name, String typed) {
+        int i = 0;
+        int j = 0;
+        while (j < typed.length()) {
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+            i++;
+            j++;
+            }else if(j>0 && typed.charAt(j) == typed.charAt(j-1)){
+            j++;
+            } else{
+                return false;
+            }
+        }
+
+        if(i == name.length()){
+            return true;
+        }
+
+        return false;
+
+    }
+
+}
